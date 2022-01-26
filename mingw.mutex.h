e13b6b38b20e746681e0e74b84c5f6e863488848
Nathaniel J. McClatchey, Ph.D <njmcclatchey@gmail.com>
2022-01-25 21:00:49 -0600

    Slight improvement to call_once
    With double-checked locking, the first check must have acquire ordering
    (to ensure that the results of the last-run function are available if
    call_once exits early, but the second does not require this (acquiring
    the mutex is also sufficient).

--- mingw.mutex.h
+++ mingw.mutex.h
@@ -465,7 +465,7 @@ void call_once(once_flag& flag, Callable&& func, Args&&... args)
     if (flag.mHasRun.load(std::memory_order_acquire))
         return;
     lock_guard<decltype(flag.mMutex)> lock(flag.mMutex);
-    if (flag.mHasRun.load(std::memory_order_acquire))
+    if (flag.mHasRun.load(std::memory_order_relaxed))
         return;
     detail::invoke(std::forward<Callable>(func),std::forward<Args>(args)...);
     flag.mHasRun.store(true, std::memory_order_release);
